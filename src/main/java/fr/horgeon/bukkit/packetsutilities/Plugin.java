36b22908541fa354302bd908b3b2b21993408faf
Horgeon <me@horgeon.fr>
2017-05-07 18:41:08 +0200

    Adding plugin configuration and main class

---
+++ src/main/java/fr/horgeon/bukkit/packetsutilities/Plugin.java
@@ -0,0 +1,11 @@
+package fr.horgeon.bukkit.packetsutilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	@Override
+	public void onEnable() {}
+
+	@Override
+	public void onDisable() {}
+}
